81489ef9ae3f6738efd10ec8703c3dcdacc393dc
Shivani Gupta <shivaniguptaa2@gmail.com>
2023-10-08 17:45:20 +0530

    added staticvar program

--- staticvar.java
+++ staticvar.java
@@ -3,6 +3,14 @@ class Mobile{
     String brand;
     int price;
     static String name;
+
+    public void show(){
+        System.out.println(brand+ " : " +price+ " : "+name);
+    }
+
+    public static void show1(Mobile obj){
+         System.out.println(obj.brand+ " : " +obj.price+ " : "+name);
+    }
 }
 
 class staticvar{
@@ -22,13 +30,20 @@ class staticvar{
     obj3.price = 25000;
     Mobile.name = "Smartphone";
 
-    Mobile obj[] = new Mobile[3];
-    obj[0] = obj1;
-    obj[1] = obj2;
-    obj[2] = obj3;
-    for(Mobile mob: obj){
-         System.out.println(mob.brand+ " : "+ mob.price+ " : " +mob.name);
-    }
+    // obj1.show1();
+//  obj3.show1();
+    Mobile.show1(obj1);
+    //enhanced for loop
+    // Mobile obj[] = new Mobile[3];
+    // obj[0] = obj1;
+    // obj[1] = obj2;
+    // obj[2] = obj3;
+    // for(Mobile mob: obj){
+    //      System.out.println(mob.brand+ " : "+ mob.price+ " : " +mob.name);
+    // }
+
+    //static variables and methods can be directly call by there class name. 
+    //Non static variables can not be called under static method.
 
     }
 }
